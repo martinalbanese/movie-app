1f8eb229c9a8ea899c4b8aa6a4319a177ecf47a6
martina <albanesemartinax@gmail.com>
2024-03-07 23:05:55 +0100

    sistemato funzione printMedia

--- js/api/printMedia.js
+++ js/api/printMedia.js
@@ -2,42 +2,40 @@ export const printMedia = async (element) => {
    switch(element.media_type) {
         case 'movie':
             console.log(
-            `ID:${media.id},\n` +
-            `Poster Path:${media.poster_path},\n` +
-            `Title:${media.title},\n` +
-            `Vote Average:${media.vote_average},\n` +
-            `Media Type:${media.media_type},\n` +
-            `Release Date:${media.release_date}\n`)
+            `ID:${element.id},\n` +
+            `Poster Path:${element.poster_path},\n` +
+            `Title:${element.title},\n` +
+            `Vote Average:${element.vote_average},\n` +
+            `Media Type:${element.media_type},\n` +
+            `Release Date:${element.release_date}\n`)
         break;
   
         case 'tv':
             console.log(
-                `ID:${media.id},\n` +
-                `Poster Path:${media.poster_path},\n` +
-                `Title:${media.title},\n` +
-                `Vote Average:${media.vote_average},\n` +
-                `Media Type:${media.media_type},\n` +
-                `Release Date:${media.release_date}\n`)
+                `ID:${element.id},\n` +
+                `Poster Path:${element.poster_path},\n` +
+                `Title:${element.name},\n` +
+                `Vote Average:${element.vote_average},\n` +
+                `Media Type:${element.media_type},\n` +
+                `Release Date:${element.first_air_date}\n`)
         break;
     
         case 'person': 
             console.log(
-                `ID:${media.id},\n` +
-                `Poster Path:${media.poster_path},\n` +
-                `Title:${media.title},\n` +
-                `Vote Average:${media.vote_average},\n` +
-                `Media Type:${media.media_type},\n` +
-                `Release Date:${media.release_date}\n`)
+                `ID:${element.id},\n` +
+                `Poster Path:${element.profile_path},\n` +
+                `Title:${element.name},\n` +
+                `Media Type:${element.media_type},\n`)
         break;
 
         default:
             console.log(
-                `ID:${media.id},\n` +
-                `Poster Path:${media.poster_path},\n` +
-                `Title:${media.title},\n` +
-                `Vote Average:${media.vote_average},\n` +
-                `Media Type:${media.media_type},\n` +
-                `Release Date:${media.release_date}\n`)
+                `ID:${element.id},\n` +
+                `Poster Path:${element.poster_path},\n` +
+                `Title:${element.title},\n` +
+                `Vote Average:${element.vote_average},\n` +
+                `Media Type:${element.media_type},\n` +
+                `Release Date:${element.release_date}\n`)
             break;
   } 
 }
